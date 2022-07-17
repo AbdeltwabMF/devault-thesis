62fb013f14372e6d522ded84183d8f123d485d15
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-07-17 18:59:25 +0200

    Update foreword

--- thesis/auto/main.el
+++ thesis/auto/main.el
@@ -4,16 +4,16 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("report" "12pt")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "headers"
@@ -26,6 +26,7 @@
     "parts/foreword"
     "parts/foreword/letter_of_approval"
     "parts/foreword/statement"
+    "parts/foreword/pre-acknowledgements"
     "parts/foreword/acknowledgements"
     "parts/foreword/abstract"
     "parts/foreword/table_of_contents"
@@ -35,12 +36,11 @@
     "parts/main/greatest_common_divisor"
     "parts/main/conclusion"
     "parts/afterword"
+    "parts/afterword/lists"
+    "parts/afterword/glossaries"
+    "parts/afterword/lists/list_of_figures"
+    "parts/afterword/lists/list_of_tables"
     "parts/afterword/references"
-    "parts/afterword/appendix"
-    "parts/afterword/table_of_figures"
-    "parts/afterword/table_of_listings"
-    "parts/afterword/list_of_tables"
-    "parts/afterword/full_bibliography"
     "report"
     "rep12"))
  :latex)
