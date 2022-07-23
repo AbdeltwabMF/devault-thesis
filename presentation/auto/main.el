bb4dc3ff181c220d39811c01d4f80017bbe35a48
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-07-23 08:00:26 +0200

    Add background materials

---
+++ presentation/auto/main.el
@@ -0,0 +1,72 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "aspectratio=169" "usenames" "dvipsnames" "pdftex")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ccicons" "scale=2")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "syntax"
+    "beamer"
+    "beamer10"
+    "listings"
+    "xcolor"
+    "multicol"
+    "media9"
+    "siunitx"
+    "ccicons"
+    "caption"
+    "subcaption"
+    "tabularx"
+    "lmodern"
+    "anyfontsize"
+    "fontawesome5"
+    "booktabs"
+    "appendixnumberbeamer"
+    "csquotes"
+    "pgfplots"
+    "tikz"
+    "xspace"
+    "amsmath"
+    "amssymb")
+   (TeX-add-symbols
+    '("Item" 1)
+    "themename")
+   (LaTeX-add-labels
+    "img:ipfsalgorithmvis1"
+    "img:ipfsalgorithmvis2"
+    "img:ipfsAlgorithmVis")
+   (LaTeX-add-bibliographies
+    "biblio")
+   (LaTeX-add-xcolor-definecolors
+    "charcoal"
+    "champagne"
+    "dimgray"
+    "flax"
+    "lavendergray"
+    "lightslategray"
+    "egyptianblue"
+    "ballblue"
+    "greencssgreen"
+    "eggshell"
+    "lava"
+    "lavenderindigo"
+    "mediumred-violet"
+    "black"
+    "forestgreen"
+    "harvardcrimson"))
+ :latex)
+
